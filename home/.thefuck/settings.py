331a06eb93c7f1efa4d184c63faa358496744924
Grzegorz Inbowski <ginbowski@gmail.com>
2015-08-18 20:48:16 +0200

    Added thefuck config

---
+++ home/.thefuck/settings.py
@@ -0,0 +1,16 @@
+# ~/.thefuck/settings.py: The Fuck settings file
+#
+# The rules are defined as in the example bellow:
+#
+# rules = ['cd_parent', 'git_push', 'python_command', 'sudo']
+#
+# The default values are as follows. Uncomment and change to fit your needs.
+# See https://github.com/nvbn/thefuck#settings for more information.
+#
+
+# wait_command = 3
+require_confirmation = True
+# priority = {}
+# rules = []
+# no_colors = False
+# debug = False
